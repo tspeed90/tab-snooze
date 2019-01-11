b7142c2875ac19b93488888048a95ec3c640643b
tspeed90 <tspeed90@gmail.com>
2019-01-11 21:19:45 +0000

    sets and gets tab url data from storage

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 // code to run
 
-function alertUser(alarm) {
-  window.alert('Its been 1 minute!');
+function alertUser() {
+  chrome.storage.local.get(['tabUrl'], function(result) {
+    window.alert(result.tabUrl);
+  });
 }
 
 chrome.alarms.onAlarm.addListener(alertUser);
--- popup.js
+++ popup.js
@@ -3,4 +3,9 @@ let snoozeButton = document.getElementById('snooze');
 
 snoozeButton.addEventListener('click', function() {
   chrome.alarms.create('one-min', { delayInMinutes: 1 });
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tab) {
+    chrome.storage.local.set({ tabUrl: tab[0].url }, function() {
+      console.log('Value set to ' + tab[0].url);
+    });
+  });
 });
